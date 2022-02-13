a4bd97e3b80bf2be1077229a81c993ba46a6de70
OrionXV3 <99570944+OrionXV3@users.noreply.github.com>
2022-02-14 00:35:30 +0700

    Update Fun.py

--- Fun.py
+++ Fun.py
@@ -2,6 +2,67 @@
 #OrionX
 
 
-import base64
-Orion = 'aW1wb3J0IG9zLCBzeXMsIHRpbWUsIHJhbmRvbQoKCksgPSAnXDAzM1s5M20nClUgPSAnXDAzM1s5NW0nCkMgPSAnXDAzM1s5NG0nClIgPSAnXDAzM1s5MW0nCkggPSAnXDAzM1s5Mm0nCgoKZnVuID0gcmFuZG9tLmNob2ljZSgoIk1lbnlhdGFrYW4gQ2ludGEgS2VwYWRhIiwiTWVuYXJpayIsIk1lbmdnaWdpdCIsIk1lbmNpdW0iLCJNZW11a3VsIiwiTWVuYW1wYXIiLCJNZW5qYXVoaSIsIk1lbmRla2F0aSIpKQoKbmFtYSA9IHN5cy5hcmd2WzFdCmRpYSA9IHN5cy5hcmd2WzJdCnBhc3N3b3JkID0gc3lzLmFyZ3ZbM10KaWYgcGFzc3dvcmQgPT0gIk9yaW9uIjoKICAgIHByaW50KEgrIlBhc3N3b3JkIEFudHVtIEJlbmFyISEiKQogICAgdGltZS5zbGVlcCgyKQplbHNlIDoKICAgIHByaW50KFIrIkFudHVtIFNhbGFoIE1lbWFzdWtrYW4gUGFzc3dvcmQsIE51bmdndW55YSBMYW1hIExvaCIpCiAgICB0aW1lLnNsZWVwKDEwMDAwMCkKb3JnaXAgPW5hbWEKCmRlZiBwcmludF9TbG93KHN0cik6CiAgICBmb3IgbGV0dGVyIGluIHN0cjoKICAgICAgICBzeXMuc3Rkb3V0LndyaXRlKGxldHRlcikKICAgICAgICBzeXMuc3Rkb3V0LmZsdXNoKCkKICAgICAgICB0aW1lLnNsZWVwKDAuMSkKCgpkZWYgcHJpbnRfU2xvd1R5cGUoc3RyKToKICAgIGZvciBsZXR0ZXIgaW4gc3RyOgogICAgICAgIHN5cy5zdGRvdXQud3JpdGUobGV0dGVyKQogICAgICAgIHN5cy5zdGRvdXQuZmx1c2goKQogICAgICAgIHRpbWUuc2xlZXAoMC4yKQoKCm9zLnN5c3RlbSgnY2xlYXInKQoKcHJpbnRfU2xvd1R5cGUoUisiV2FpdC4uLi4uXG4iKQpwcmludChVKyJbICAgICAgICAgICAgICAgICAgICBdIDAlXG4gIikKdGltZS5zbGVlcCgxKQpwcmludF9TbG93KFUrIls9PT09PSAgICAgICAgICAgICAgIF0gMjUlXG4iKQp0aW1lLnNsZWVwKDEpCnByaW50X1Nsb3coVSsiWz09PT09PT09PT0gICAgICAgICAgXSA1MCVcbiIpCnRpbWUuc2xlZXAoMSkKcHJpbnRfU2xvdyhVKyJbPT09PT09PT09PT09PT09ICAgICBdIDc1JVxuIikKdGltZS5zbGVlcCgxKQpwcmludF9TbG93KFUrIls9PT09PT09PT09PT09PT09PT09PV0gMTAwJVxuIikKdGltZS5zbGVlcCgzKQoKb3Muc3lzdGVtKCdjbGVhcicpCnByaW50X1Nsb3dUeXBlKEgrIldlbGNvbWUsIEFyZSBZb3UgUmVhZHk/IikKdGltZS5zbGVlcCgzKQpvcy5zeXN0ZW0oJ2NsZWFyJykKCnByaW50X1Nsb3dUeXBlKEMrIlxuJXMgIiUob3JnaXApKQpwcmludF9TbG93VHlwZShmdW4'
-exec(base64.b64decode(Orion))
+import os, sys, time, random
+
+
+K = '\033[93m'
+U = '\033[95m'
+C = '\033[94m'
+R = '\033[91m'
+H = '\033[92m'
+
+
+fun = random.choice(("Menyatakan Cinta Kepada","Menarik","Menggigit","Mencium","Memukul","Menampar","Menjauhi","Mendekati"))
+
+nama = sys.argv[1]
+dia = sys.argv[2]
+password = sys.argv[3]
+if password == "Orion":
+    print(H+"Password Antum Benar!!")
+    time.sleep(2)
+else :
+    print(R+"Antum Salah Memasukkan Password, Nunggunya Lama Loh")
+    time.sleep(100000)
+orgip =nama
+
+def print_Slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+def print_SlowType(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.2)
+
+
+os.system('clear')
+
+print_SlowType(R+"Wait.....\n")
+print(U+"[                    ] 0%\n ")
+time.sleep(1)
+print_Slow(U+"[=====               ] 25%\n")
+time.sleep(1)
+print_Slow(U+"[==========          ] 50%\n")
+time.sleep(1)
+print_Slow(U+"[===============     ] 75%\n")
+time.sleep(1)
+print_Slow(U+"[====================] 100%\n")
+time.sleep(3)
+
+os.system('clear')
+print_SlowType(H+"Welcome, Are You Ready?")
+time.sleep(3)
+os.system('clear')
+
+print_SlowType(C+"\n%s "%(orgip))
+print_SlowType(fun +" %s"%(dia))           
+
+time.sleep(8)
+print("\n\n\n\n")
+print(H+"\nBye!")
+pass
+#Made By Orion
