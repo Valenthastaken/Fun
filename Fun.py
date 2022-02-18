3066ea5c1bf8933cdf8d920cc484fa3ea0dc72fd
Orion <hidehastaken@gmail.com>
2022-02-19 03:20:40 +0700

    Update Fun.py

--- Fun.py
+++ Fun.py
@@ -2,33 +2,33 @@
 
 import os, sys, time, random
 
-#Warna
-K = '\033[93m' #kuning
-U = '\033[95m' #Ungu
+#Color
+K = '\033[93m' #Yellow
+U = '\033[95m' #Purple
 C = '\033[94m' #biru tua
-R = '\033[91m' #merah
-H = '\033[92m' #hijau
+R = '\033[91m' #Red
+H = '\033[92m' #Green
 
-#Pilihan Random
+#Random Choice
 fun = random.choice(("Looking At","Love","Hate","Like","Lick","Say Love To","Pull","Bytes","Touching","Make Cry","Laughing Together With","Saying Hate To","Kick","Push","Hug","'s Head. Pat By","Feel Embrassing To","Receive Love From","Refusing Love From","Kiss","Punch","Slap","Approach","Stay Away From"))
 
-#Argumen Ke 1
+#First Args
 nama = sys.argv[1]
-#Argumen Ke 2
+#Second Args
 dia = sys.argv[2]
 #Alias
 name =nama
  
 
 
-#Untuk Slow Type Kata Yang Kita Print
+#Slowtype Word
 def print_SlowType(str):
     for letter in str:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.1)
 
-#Hanya Pemanis
+#Addon
 os.system('clear')
 print (R+"                      Loading                 \n")
 time.sleep(2)
@@ -36,12 +36,12 @@ z = input(H+"Press enter to start")
 os.system('clear')
 time.sleep(2)
 
-#Hidangan Utama:v
+#Main course
 print_SlowType(C+"%s "%(name))
 print_SlowType(fun +" %s"%(dia))           
 time.sleep(3)
 print("\n\n\n\n")
-#Biar Gampang
+#Easy
 x = input(K+"Press enter to exit")
   
 pass 
